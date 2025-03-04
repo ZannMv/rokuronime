71eb257f810e5bb877d627715632b1870b4e4b5d
ZannMv <116694002+ZannMv@users.noreply.github.com>
2025-03-05 05:25:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'otakudesu.cloud',
+        hostname: 'wajik-anime-api.vercel.app',
         port: '',
       },
     ],
